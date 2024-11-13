d5d66381465ca25ec7e0a231e9ce74315c29b2f2
wnqhal <jubomi050623@gmail.com>
2024-11-13 16:37:16 +0900

    [FEAT] 페이지 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,20 +1,74 @@
+import { useState } from "react";
 import styled from "styled-components";
+import cat from "../asset/imgs/laughing_cat.svg";
+import human from "../asset/imgs/laughing_man.svg";
+import { Link } from "react-router-dom";
 
 const Main = () => {
+  const [show, setShow] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setShow(true);
+  };
+
   return (
-    <Wrapper>
-      <Title>Jocker</Title>
-      <ClickAnyWhere>click anywhere</ClickAnyWhere>
+    <Wrapper onClick={handleClick} cursor={show}>
+      <TitleWrap>
+        <Title>Jocker</Title>
+      </TitleWrap>
+      <ImgWrap>
+        <img
+          className={`fade-box ${show && "shake"}`}
+          src={cat}
+          alt="laughing cat"
+        />
+        <img
+          className={`fade-box ${show && "shake"}`}
+          src={human}
+          alt="laughing man"
+        />
+      </ImgWrap>
+      {show ? (
+        <Link to={"/joke"}>
+          <Start>START</Start>
+        </Link>
+      ) : (
+        <HelperText>click anywhere</HelperText>
+      )}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<{ cursor: boolean }>`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
   height: 100%;
+  cursor: ${({ cursor }) => (cursor ? "default" : "pointer")};
+`;
+
+const TitleWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+  gap: 12px;
+`;
+
+const ImgWrap = styled.div`
+  position: absolute;
+  & > img {
+    position: absolute;
+  }
+  & > img:first-child {
+    top: 80px;
+    left: -330px;
+  }
+  & > img:last-child {
+    top: 100px;
+    left: 160px;
+  }
 `;
 
 const Title = styled.h1`
@@ -22,9 +76,16 @@ const Title = styled.h1`
   padding-top: 60px;
 `;
 
-const ClickAnyWhere = styled.p`
+const HelperText = styled.p`
   font-size: 20px;
   padding-bottom: 65px;
+`;
+
+const Start = styled.p`
+  font-size: 30px;
+  text-decoration: underline;
+  padding-bottom: 65px;
+  cursor: pointer;
 `;
 
 export default Main;
